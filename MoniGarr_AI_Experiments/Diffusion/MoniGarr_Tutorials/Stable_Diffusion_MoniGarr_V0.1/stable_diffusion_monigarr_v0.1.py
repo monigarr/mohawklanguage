378b55684e61513b66bd38af8f67ad753fd7c355
MoniGarr <monigarr@monigarr.com>
2022-11-05 18:49:54 -0400

    added Tutorial Info for Diffusion local runs

---
+++ MoniGarr_AI_Experiments/Diffusion/MoniGarr_Tutorials/Stable_Diffusion_MoniGarr_V0.1/stable_diffusion_monigarr_v0.1.py
@@ -0,0 +1,35 @@
+#--------------------------------------------------------
+#
+#  Stable Diffusion Tutorial
+#  from MoniGarr.com
+#  Tutorial Link: https://wp.me/p2mXO3-1gF
+# 
+#--------------------------------------------------------
+
+#-----------------------------------------------------
+# imports
+#-----------------------------------------------------
+import time
+import keras_cv
+from tensorflow import keras
+import matplotlib.pyplot as plt
+
+model = keras_cv.models.StableDiffusion(img_width=512, img_height=512)
+
+#------------------------------------------------------
+# Prompt String 
+# "Forest landsape with distant river"
+# Change prompt string to anything you imagine.
+#------------------------------------------------------
+images = model.text_to_image("Forest landscape with distant river", batch_size=1)
+
+def plot_images(images):
+    plt.figure(figsize=(20, 20))
+    for i in range(len(images)):
+        ax = plt.subplot(1, len(images), i + 1)
+        plt.imshow(images[i])
+        plt.axis("off")
+
+plot_images(images)
+
+plt.show()
